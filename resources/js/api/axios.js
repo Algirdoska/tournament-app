4ebb209ea6edba32a33a17068694a5e689635018
Algirdas Pocius <algirdas.pocius1@gmail.com>
2022-12-19 02:18:31 +0200

    oops

--- resources/js/api/axios.js
+++ resources/js/api/axios.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3069/api",
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_BASE_URL,
+    withCredentials: true,
 });
 
 axiosConfig.interceptors.request.use(
-    config => {
-      config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
-          return config;
-      },
-      error => {
-          return Promise.reject(error);
-      }
-  );
+    (config) => {
+        config.headers["Authorization"] = `Bearer ${localStorage.getItem(
+            "token"
+        )}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
-export default axiosConfig
+export default axiosConfig;
